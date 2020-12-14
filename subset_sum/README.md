185cd69426f9cc0f41cbb12e96d4e18e94956d0f
franciscomarmolejo <francisco.marmolejo@metricamovil.com>
2020-12-14 17:48:01 -0600

    Subset sum

--- subset_sum/README.md
+++ subset_sum/README.md
@@ -1,6 +1,6 @@
 # Subset sum problem
 ## Given an array of N integers (ex: [1,2,3,5,7,13]) determine if there exists a subset whose sum is equal to given value (ex: 8). 
-### For this example, subset1: [3,5] and subset2: [1,7]
+### For this example, a subset indeed exist ([3,5] or [1,7]
 ### Original problem: https://thecodingsimplified.com/subset-sum-problem/
 
 
--- subset_sum/subset-sum.js
+++ subset_sum/subset-sum.js
@@ -1,5 +1,31 @@
-let subset_sum = (arr, total) => {
+let subset_sum = (arr, wanted) => {
+    arr.sort((a,b)=> a - b);
+    const rows = arr.length
+    const cols = wanted + 1;
+    let memo = new Array(rows);
+    for(let i =0; i<rows;i++){
+        memo[i]= new Array(cols);
+    }
+    for(let i=0;i<rows;i++){
+        memo[i][0]= true;
+    }
+    for(let j=0; j<cols;j++){
+        if(j==arr[0]){
+            memo[0][j]=true;
+        }  
+    }
+    for(let i=1; i<rows;i++){
+        for(let j=1; j<cols; j++){
+            if(memo[i-1][j]){
+                memo[i][j]=true;
+            }
+            else{
+                if(j>=arr[i]){
+                    memo[i][j]= memo[i-1][j - arr[i]]
+                }
+            }
+        }
+    }
 
+    return console.log(memo[rows-1][cols-1])
 };
-
-subset_sum([1, 2, 3, 5], 7);
